8e2479a5c98dddddedc5012ea0e208ab8b57d6ad
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2022-08-01 12:24:36 -0600

    build user interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
   root "movies#index"
+  get "signup" => "users#new"
+  resources :users
   resources :movies do
     resources :reviews 
   end
